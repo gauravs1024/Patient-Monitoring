60848c69a1f19f4b3c33cf7b2e5fe8dbca830db9
GAURAV <gauravs1024@gmail.com>
2023-11-03 20:49:00 +0530

    version 1.0

--- app/src/main/java/com/example/patientmonitoring/MainActivity.java
+++ app/src/main/java/com/example/patientmonitoring/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.patientmonitoring;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText name=findViewById(R.id.name);
+        EditText age=findViewById(R.id.age);
+        Button btnlogin=findViewById(R.id.btnlogin);
+
+
+
     }
 }
